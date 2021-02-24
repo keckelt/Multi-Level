f8b3064e781bb7626386f76142a417b16b8a8f48
Klaus Eckelt <Klaus.Eckelt@jku.at>
2021-02-24 16:59:24 +0100

    fix R script

--- r/toLong.R
+++ r/toLong.R
@@ -4,11 +4,12 @@ df <- data.frame(soccer_strikers)
 
 # duplicate entries for ‘Leandrinho.12’, ‘Wanderson.12’, ‘William.12’
 # --> renamed them to <Name> A and B
-df_long <- reshape(df, direction='long', 
-        varying=c('games12', 'games13', 'games14', 'games15', 'games16', 'games17', 'goals12', 'goals13', 'goals14', 'goals15', 'goals16', 'goals17', 'assists12', 'assists13', 'assists14', 'assists15', 'assists16', 'assists17', 'minutes12', 'minutes13', 'minutes14', 'minutes15', 'minutes16', 'minutes17'), 
+df_long <- reshape(df, 
+        direction='long', 
+        varying=9:32,
         timevar='year',
         times=c('12', '13', '14', '15', '16', '17'),
-        v.names=c('games', 'goals', 'assists', 'minutes'),
+        sep="",
         idvar='player')
 
-write.csv(df_long,"soccer-long.csv", row.names = FALSE)
+write.csv(df_long,"soccer-long.csv", row.names = FALSE, na="")
